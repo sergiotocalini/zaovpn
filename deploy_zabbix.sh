90286cedea79ac399233dfd2c0c3ee03462bfe6a
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-09 14:16:51 +0100

    Fixing deploy_zabbix.sh script.

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -5,6 +5,7 @@ ZABBIX_DIR=/etc/zabbix
 OPENVPN_LISTEN="${1:-0.0.0.0}"
 OPENVPN_PORT="${2:-1194}"
 OPENVPN_STATUS="${3:-/etc/openvpn/openvpn-status.log}"
+OPENVPN_CCD="${4:-/etc/openvpn/ccd}"
 
 mkdir -p ${ZABBIX_DIR}/scripts/agentd/zaovpn
 cp ${SOURCE_DIR}/zaovpn/zaovpn.conf.example ${ZABBIX_DIR}/scripts/agentd/zaovpn/zaovpn.conf
@@ -13,3 +14,4 @@ cp ${SOURCE_DIR}/zaovpn/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/zaovpn.
 sed -i "s|OPENVPN_LISTEN=.*|OPENVPN_LISTEN=\"${OPENVPN_LISTEN}\"|g" ${ZABBIX_DIR}/scripts/agentd/zaovpn/zaovpn.conf
 sed -i "s|OPENVPN_PORT=.*|OPENVPN_PORT=\"${OPENVPN_PORT}\"|g" ${ZABBIX_DIR}/scripts/agentd/zaovpn/zaovpn.conf
 sed -i "s|OPENVPN_STATUS=.*|OPENVPN_STATUS=\"${OPENVPN_STATUS}\"|g" ${ZABBIX_DIR}/scripts/agentd/zaovpn/zaovpn.conf
+sed -i "s|OPENVPN_CCD=.*|OPENVPN_CCD=\"${OPENVPN_CCD}\"|g" ${ZABBIX_DIR}/scripts/agentd/zaovpn/zaovpn.conf
