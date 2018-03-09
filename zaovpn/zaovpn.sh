bd57c4c026aaa79ddc78007ee6660d8e00bca4ce
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-03-09 11:29:31 +0100

    Adding initial scripts

---
+++ zaovpn/zaovpn.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env ksh
+rcode=0
+PATH=/usr/local/bin:${PATH}
+
+#################################################################################
+
+#################################################################################
+#
+#  Variable Definition
+# ---------------------
+#
+APP_NAME=$(basename $0)
+APP_DIR=$(dirname $0)
+APP_VER="0.0.1"
+APP_WEB="http://www.sergiotocalini.com.ar/"
+#
+#################################################################################
+
+#################################################################################
+#
+#  Load Environment
+# ------------------
+#
+[[ -f ${APP_DIR}/${APP_NAME%.*}.conf ]] && . ${APP_DIR}/${APP_NAME%.*}.conf
+
+#
+#################################################################################
+
+#################################################################################
+#
+#  Function Definition
+# ---------------------
+#
+usage() {
+    echo "Usage: ${APP_NAME%.*} [Options]"
+    echo ""
+    echo "Options:"
+    echo "  -a            Query arguments."
+    echo "  -h            Displays this help message."
+    echo "  -j            Jsonify output."
+    echo "  -s ARG(str)   Section (default=stat)."
+    echo "  -v            Show the script version."
+    echo ""
+    echo "Please send any bug reports to sergiotocalini@gmail.com"
+    exit 1
+}
+
+version() {
+    echo "${APP_NAME%.*} ${APP_VER}"
+    exit 1
+}
+
+get_status() {
+    file=${1:-/etc/openvpn/openvpn-status.log}
+    index=${2}
+    qfilter=${3}
+    
+    raw=`awk "/CLIENT LIST/,/ROUTING TABLE/" ${file} | tail -n +4 | head -n -1`
+    if [[ -z ${qfilter} ]]; then
+	raw=`grep "${qfilter}" ${raw}`
+    fi
+    res=`echo ${raw} | awk -F, '{s+=\$index} END {print s}'`
+    echo ${res}
+}
+#
+#################################################################################
+
+#################################################################################
+while getopts "s::a:s:uphvj:" OPTION; do
+    case ${OPTION} in
+	h)
+	    usage
+	    ;;
+	s)
+	    SECTION="${OPTARG}"
+	    ;;
+        j)
+            JSON=1
+            IFS=":" JSON_ATTR=(${OPTARG//p=})
+            ;;
+	a)
+	    ARGS[${#ARGS[*]}]=${OPTARG//p=}
+	    ;;
+	v)
+	    version
+	    ;;
+         \?)
+            exit 1
+            ;;
+    esac
+done
+
+
+if [[ ${JSON} -eq 1 ]]; then
+    rval=$(discovery ${SECTION} ${ARGS[*]})
+    echo '{'
+    echo '   "data":['
+    count=1
+    while read line; do
+        IFS="|" values=(${line})
+        output='{ '
+        for val_index in ${!values[*]}; do
+            output+='"'{#${JSON_ATTR[${val_index}]:-${val_index}}}'":"'${values[${val_index}]}'"'
+            if (( ${val_index}+1 < ${#values[*]} )); then
+                output="${output}, "
+            fi
+        done 
+        output+=' }'
+        if (( ${count} < `echo ${rval}|wc -l` )); then
+            output="${output},"
+        fi
+        echo "      ${output}"
+        let "count=count+1"
+    done <<< ${rval}
+    echo '   ]'
+    echo '}'
+else
+    rval=$( get_stat ${SECTION} ${ARGS[*]} )
+    rcode="${?}"
+    echo ${rval:-0}
+fi
+
+exit ${rcode}
