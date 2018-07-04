d06988395d15e3db91bbedc40ce2353bf00fe2c6
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-07-04 15:42:30 +0200

    Fixing certs discovery.

--- zaovpn/zaovpn.sh
+++ zaovpn/zaovpn.sh
@@ -144,7 +144,7 @@ discovery() {
 	cafile=`sudo grep -E "^ca " "${OPENVPN_CONF}" | awk '{print $2}'`
 	crlfile=`sudo grep -E "^crl-verify " "${OPENVPN_CONF}" | awk '{print $2}'`
 	if [[ -z ${OPENVPN_CERTS_LIST} ]]; then
-	    certs_files=`sudo find "${OPENVPN_CERTS}" -name "*.crt" -print|sort 2>/dev/null`
+	    certs_files=`sudo find "${OPENVPN_CERTS}" -name "*.crt" -print 2>/dev/null|sort 2>/dev/null`
 	else
 	    while read line; do
 		file=`sudo find "${OPENVPN_CERTS}" -name "${line}*.crt" -print -quit 2>/dev/null`
